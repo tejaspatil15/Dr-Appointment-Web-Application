9d9237cbc4a582c73336471715ab74db414aab6b
Tejas Patil <tejasp163@gmail.com>
2021-12-24 03:02:43 +0530

    comiit at 3:02Am

--- app.js
+++ app.js
@@ -17,7 +17,10 @@ const PORT = process.env.PORT || 5000;
 
 if (process.env.NODE_ENV == "production"){
     app.use(express.static("homepage/build"));
-    // const path =require("path");
+    const path =require("path");
+    app.get("*",(req,res)=>{
+        res.sendFile(path.resolve(__dirname,'homepage','build','index.html'));
+    })
 }
 
 app.listen(PORT, () => {
